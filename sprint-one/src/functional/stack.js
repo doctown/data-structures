3cb4605f0376afa555a8fb3e29bf5aea61e41c94
Nundy <nundy@Nundys-MacBook-Pro.local>
2016-03-02 12:24:17 -0800

    Items can be added and remove from the stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -7,9 +7,19 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[items] = value;
+    items++;
   };
 
   someInstance.pop = function() {
+    if (items >= 0) {
+      item--;
+      return storage[items];
+    } else {
+      // should an error be thrown if the stack is popped while empty
+      return undefined;
+    }
+
   };
 
   someInstance.size = function() {
