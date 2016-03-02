2cc8f1b17fcd6b2a31367436f33574ba75d19289
doctown <doctown@yahoo.com>
2016-03-02 15:51:09 -0800

    Set up functional pattern, report size of initial stack.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  obj.stackSize = 0;
+  obj.push = stackMethods.push;
+  obj.pop = stackMethods.pop;
+  obj.size = stackMethods.size;
+
+  return obj;
 };
 
 var stackMethods = {};
+stackMethods.push = function() {
 
+};
 
+stackMethods.pop = function() {
+
+};
+
+stackMethods.size = function() {
+  return this.stackSize;
+};
