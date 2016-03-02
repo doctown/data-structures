b857fa2674dd0be5beef1f82e51ce803cc1bf2fd
Hack Reactor Students <accounts@hackreactor.com>
2016-03-02 14:55:42 -0800

    Enqueue an item in the queue and incrment the size

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -8,6 +8,8 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[queueSize] = value;
+    queueSize++;
   };
 
   someInstance.dequeue = function() {
