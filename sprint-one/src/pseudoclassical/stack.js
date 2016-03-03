b9f9bc563e6d96a832f59a6a0ba8df0c0d5527f9
Hack Reactor Students <accounts@hackreactor.com>
2016-03-03 10:16:19 -0800

    Removed the obect and added directly to the prototype

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -3,26 +3,24 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
   this.stackSize = 0;
   this.stack = {};
-
-  return this;
 };
 
-Stack.prototype = {
-  push: function (value) {
-    this.stack[this.stackSize] = value;
-    this.stackSize++;  
-  },
-  pop: function () {
-    if (this.stackSize > 0) {
-      this.stackSize--;
-      var result = this.stack[this.stackSize];
-      delete this.stack[this.stackSize];
-    }
-    return result;
-  }, 
-  size: function () {
-    return this.stackSize;
+Stack.prototype.push = function (value) {
+  this.stack[this.stackSize] = value;
+  this.stackSize++;  
+};
+
+Stack.prototype.pop = function () {
+  if (this.stackSize > 0) {
+    this.stackSize--;
+    var result = this.stack[this.stackSize];
+    delete this.stack[this.stackSize];
   }
+  return result;
+};
+
+Stack.prototype.size = function () {
+  return this.stackSize;
 };
 
 
