4de2effab2a5b14b711d08387f6c142c99345882
Hack Reactor Students <accounts@hackreactor.com>
2016-03-03 10:06:14 -0800

    Return the size of the stack and push an element

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.stackSize = 0;
+  this.stack = {};
+
+  return this;
 };
+
+Stack.prototype = {
+  push: function (value) {
+    this.stack[this.stackSize] = value;
+    this.stackSize++;  
+  },
+  pop: function () {
+
+  }, 
+  size: function () {
+    return this.stackSize;
+  }
+};
+
 
 
