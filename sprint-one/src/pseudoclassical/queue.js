9d1849dbdf76530bd5b4f5c34397879d740e0dab
doctown <doctown@yahoo.com>
2016-03-03 10:19:58 -0800

    Report queue size

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,19 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.queue = {};
+  this.queueSize = 0;
 };
 
+Queue.prototype.push = function(item) {
 
+};
+
+Queue.prototype.pop = function () {
+  
+};
+
+Queue.prototype.size = function () {
+  return this.queueSize;
+};
