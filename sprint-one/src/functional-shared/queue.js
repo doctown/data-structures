75208bbea38be9ee721f381daff06efb37cfd84b
doctown <doctown@yahoo.com>
2016-03-02 16:08:49 -0800

    Return the size of the queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  obj.queueSize = 0;
+
+  obj.enqueue = queueMethods.enqueue;
+  obj.dequeue = queueMethods.dequeue;
+  obj.size = queueMethods.size;
+
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+};
 
+queueMethods.enqueue = function(value) {
 
+};
+
+queueMethods.dequeue = function() {
+
+};
+
+queueMethods.size = function() {
+  return this.queueSize;
+};
