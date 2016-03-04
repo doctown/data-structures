1b1dd101d1b4b1c98258184cd5d1d35179b276a6
Hack Reactor Students <accounts@hackreactor.com>
2016-03-04 10:59:29 -0800

    Refactor hash array to store array rather than object

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,29 +5,57 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // Key-value pairs are stored in an array and added as pair to the array if it exists
   if (this._storage.get(index) === undefined) {
-    var obj = {};
-    obj[k] = v;
-    this._storage.set(index, obj);  
+    var stored = [];
+    stored.push(k);
+    stored.push(v);
+    this._storage.set(index, stored);  
   } else {
-    var obj = this._storage.get(index);
-    obj[k] = v;
+    var stored = this._storage.get(index);
+    var isFound = false;
+
+    for (var i = 0; i < stored.length; i += 2) {
+      if (stored[i] === k) {
+        stored[i + 1] = v;
+        isFound = true;
+      }
+    }
+    
+    if (!isFound) {
+      stored.push(k);
+      stored.push(v);
+    }
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var obj = this._storage.get(index);
-  return obj[k];
+  var stored = this._storage.get(index);
+  var retVal = undefined;
+
+  for (var i = 0; i < stored.length; i += 2) {
+    if (stored[i] === k) {
+      retVal = stored[i + 1];
+    }
+  }
+
+  return retVal;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var obj = this._storage.get(index);
-  var res = obj[k];
-  delete obj[k];
+  var stored = this._storage.get(index);
+  var retVal = undefined;
+  
+  for (var i = 0; i < stored.length; i += 2) {
+    if (stored[i] === k) {
+      retVal = stored[i + 1];
+      stored.splice(i, 2);
+    }
+  }
 
-  return res;
+  return retVal;
 };
 
 
