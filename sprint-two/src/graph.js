c4fd1f5efc365298dbd2c5fc9d52d746ca67c545
doctown <doctown@yahoo.com>
2016-03-03 16:21:25 -0800

    Add a node and remove a node

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,21 +3,29 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
+  this.graph = {};
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.graph[node] = [];
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return Object.keys(this.graph).indexOf(node.toString()) !== -1;
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  _.each(this.graph[node], function (item) {
+    var itemIndex = this.graph[item].indexOf(node);
+    this.graph[item].splice(itemIndex, 1);
+  });
+  delete this.graph[node];
 };
 
 // ------------------------
