5dbcecbcf8c1130f902591dafddaadc6c97338e6
doctown <doctown@yahoo.com>
2016-03-05 12:43:02 -0800

    Impliment insert and contains methods

--- sprint-advanced/src/bloomFilter.js
+++ sprint-advanced/src/bloomFilter.js
@@ -1,7 +1,7 @@
 var BloomFilter = function () {
-  var initialSize = 18;
-  this.limitedArray = new LimitedArray(initialSize);
-  for (var i = 0; i < initialSize; i++) {
+  this.capacity = 18;
+  this.limitedArray = new LimitedArray (this.capacity);
+  for (var i = 0; i < this.capacity; i++) {
     this.limitedArray.set(i, false);
   } 
 
@@ -9,9 +9,29 @@ var BloomFilter = function () {
 };
 
 BloomFilter.prototype.insert = function (value) {
-  // 
+  // store each hash function in a variable
+    // call the hash
+  var that = this;
+
+  hashFunctions.forEach(function (iterator) { // for each hash function
+    // set the limitedArray at the index of the resulted hash value to true
+
+    that.limitedArray.set(iterator(value, that.capacity), true);
+  });
 };
 
 BloomFilter.prototype.contains = function(value) {
+  var isFound = false;
+  var that = this;
 
+  // loop through each hash function 
+  hashFunctions.forEach(function (iterator) {
+    // get the index of the hash value
+      // if any return true, return true
+    if (that.limitedArray.get(iterator(value, that.capacity))) {
+      isFound = true;
+    }
+  });
+
+  return isFound;
 };
