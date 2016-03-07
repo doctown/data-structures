df9fac3c2cf8e50976dafc93bb9cb1c024043555
David Ogor <doctown@yahoo.com>
2016-03-06 18:00:43 -0800

    Add test cases for addToHead and removeTail methods

--- sprint-advanced/spec/doublyLinkedListSpec.js
+++ sprint-advanced/spec/doublyLinkedListSpec.js
@@ -2,7 +2,7 @@ describe('doublyLinkedList', function() {
   var doublyLinkedList;
 
   beforeEach(function() {
-    doublyLinkedList = doublyLinkedList();
+    doublyLinkedList = DoublyLinkedList();
   });
 
   it('should have a head and tail', function() {
@@ -10,10 +10,20 @@ describe('doublyLinkedList', function() {
     expect(doublyLinkedList).to.have.property('tail');
   });
 
-  it('should have methods named "addToTail", "removeHead", and "contains"', function() {
+  it('should have methods named "addToHead", "addToTail", "removeHead", "removeTail", and "contains"', function() {
+    expect(doublyLinkedList.addToHead).to.be.a('function');
     expect(doublyLinkedList.addToTail).to.be.a('function');
     expect(doublyLinkedList.removeHead).to.be.a('function');
+    expect(doublyLinkedList.removeTail).to.be.a('function');
     expect(doublyLinkedList.contains).to.be.a('function');
+  });
+
+  it('should allow item to be added to the head', function () {
+    expect(doublyLinkedList.head).to.equal(null);
+    doublyLinkedList.addToHead(0);
+    expect(doublyLinkedList.head.value).to.equal(0);
+    doublyLinkedList.addToHead(1);
+    expect(doublyLinkedList.head.value).to.equal(1);
   });
 
   it('should designate a new tail when new nodes are added', function() {
@@ -29,6 +39,25 @@ describe('doublyLinkedList', function() {
     expect(doublyLinkedList.head.value).to.equal(4);
     doublyLinkedList.removeHead();
     expect(doublyLinkedList.head.value).to.equal(5);
+  });
+
+  it('should remove the tail and return the value stored in the removed tail when removeTail is called', function () {
+    // Error should be thrown when remove tail is called on an empty list
+    expect(doublyLinkedList.removeTail()).to.throw();
+    doublyLinkedList.addToHead(2);
+    doublyLinkedList.addToHead(1);
+    expect(doublyLinkedList.removeTail()).to.equal(2);
+    expect(doublyLinkedList.tail.value).to.equal(1);
+    doublyLinkedList.addToTail(2);
+    expect(doublyLinkedList.removeTail()).to.equal(2);
+    expect(doublyLinkedList.tail.value).to.equal(1);
+    expect(doublyLinkedList.removeTail()).to.equal(1);
+    expect(doublyLinkedList.tail.value).to.equal(null);
+    // Error should be thrown when remove tail is called on an empty list
+    expect(doublyLinkedList.removeTail()).to.throw();
+    doublyLinkedList.addToHead(0);
+    expect(doublyLinkedList.removeTail()).to.equal(0);
+    expect(doublyLinkedList.tail.value).to.equal(null);
   });
 
   it('should return the value of the former head when removeHead is called', function() {
